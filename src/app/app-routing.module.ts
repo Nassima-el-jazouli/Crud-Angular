3f70df598990f41b2c77371f44e2234ea10a0b55
Nassima EL JAZOULI <nassima.eljazouli@etu.uae.ac.ma>
2022-04-23 18:55:41 +0000

    Third commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddTutorialComponent } from './add-tutorial/add-tutorial.component';
 import { TutorialDetailsComponent } from './tutorial-details/tutorial-details.component';
+import { TutorialListComponent } from './tutorial-list/tutorial-list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'tutorials', pathMatch: 'full' },
-  { path: 'tutorials', component: AddTutorialComponent },
+  { path: 'tutorials', component: TutorialListComponent },
   { path: 'tutorials/:id', component: TutorialDetailsComponent },
   { path: 'add', component: AddTutorialComponent }
 ];
--- src/app/services/tutorial.service.ts
+++ src/app/services/tutorial.service.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Tutorial } from '../models/tutorial.model';
 
 const baseUrl = 'http://localhost:8000/api/tutorials';
 
@@ -7,5 +10,28 @@ const baseUrl = 'http://localhost:8000/api/tutorials';
 })
 export class TutorialService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Tutorial[]> {
+    return this.http.get<Tutorial[]>(baseUrl);
+  }
+
+  get(id: any): Observable<Tutorial> {
+    return this.http.get<Tutorial>(`${baseUrl}/${id}`);
+  }
+  create(data: any): Observable<any> {
+    return this.http.post(baseUrl, data);
+  }
+  update(id: any, data: any): Observable<any> {
+    return this.http.put(`${baseUrl}/${id}`, data);
+  }
+  delete(id: any): Observable<any> {
+    return this.http.delete(`${baseUrl}/${id}`);
+  }
+  deleteAll(): Observable<any> {
+    return this.http.delete(baseUrl);
+  }
+  findByTitle(title: any): Observable<Tutorial[]> {
+    return this.http.get<Tutorial[]>(`${baseUrl}?title=${title}`);
+  } 
 }
