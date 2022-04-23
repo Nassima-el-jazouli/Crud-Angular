7492068921f75146e86b2ba82b3ba593f90e37cb
Nassima EL JAZOULI <nassima.eljazouli@etu.uae.ac.ma>
2022-04-23 21:26:16 +0000

    Another commit

--- src/app/tutorial-details/tutorial-details.component.ts
+++ src/app/tutorial-details/tutorial-details.component.ts
@@ -13,13 +13,67 @@ export class TutorialDetailsComponent implements OnInit {
     title: '',
     description: '',
     published: false
-  }
+  };
+  message = ''
   constructor(
     private tutorialService: TutorialService,
     private route: ActivatedRoute,
     private router: Router) { }
 
   ngOnInit(): void {
+    this.message = '';
+    this.getTutorial(this.route.snapshot.params['id']);
   }
-  
+  getTutorial(id: string): void {
+    this.tutorialService.get(id)
+      .subscribe(
+        data => {
+          this.currentTutorial = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
+  }
+  updatePublished(status: boolean): void {
+    const data = {
+      title: this.currentTutorial.title,
+      description: this.currentTutorial.description,
+      published: status
+    };
+    this.message = '';
+    this.tutorialService.update(this.currentTutorial.id, data)
+      .subscribe(
+        response => {
+          this.currentTutorial.published = status;
+          console.log(response);
+          this.message = response.message ? response.message : 'The status was updated successfully!';
+        },
+        error => {
+          console.log(error);
+        });
+  }
+  updateTutorial(): void {
+    this.message = '';
+    this.tutorialService.update(this.currentTutorial.id, this.currentTutorial)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.message = response.message ? response.message : 'This tutorial was updated successfully!';
+        },
+        error => {
+          console.log(error);
+        });
+  }
+  deleteTutorial(): void {
+    this.tutorialService.delete(this.currentTutorial.id)
+      .subscribe(
+        response => {
+          console.log(response);
+          this.router.navigate(['/tutorials']);
+        },
+        error => {
+          console.log(error);
+        });
+  }
 }
